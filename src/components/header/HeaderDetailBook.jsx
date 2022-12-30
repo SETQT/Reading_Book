80bf0dc05619246b2c1c9bdf571eb9f4a5399d1c
20120373 <letruongkinhthanh@gmail.com>
2022-12-30 13:57:13 +0700

    thanh

--- src/components/header/HeaderDetailBook.jsx
+++ src/components/header/HeaderDetailBook.jsx
@@ -25,16 +25,7 @@ function header() {
                                 <li className="nav-item">
                                     <a className="nav-link mx-2 active" aria-current="page" href="/user">My Book</a>
                                 </li>
-                                <li className="nav-item dropdown">
-                                    <a className="nav-link mx-2 dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Company
-                                    </a>
-                                    <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                        <li><a className="dropdown-item" href="#">Blog</a></li>
-                                        <li><a className="dropdown-item" href="#">About Us</a></li>
-                                        <li><a className="dropdown-item" href="#">Contact us</a></li>
-                                    </ul>
-                                </li>
+                              
                             </ul>
                         </div>
                     </div>
