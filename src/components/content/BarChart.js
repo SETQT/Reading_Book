583c7153fa4b5346d5665eb5869f19b331d847f8
20120373 <letruongkinhthanh@gmail.com>
2022-12-31 18:17:36 +0700

    1

--- src/components/content/BarChart.js
+++ src/components/content/BarChart.js
@@ -3,7 +3,8 @@ import { Bar } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 
 function BarChart({ chartData }) {
-  return new Bar("ctx", {
+  return new ChartJS("ctx", {
+    type: 'bar',
     data: {
       chartData
     },
