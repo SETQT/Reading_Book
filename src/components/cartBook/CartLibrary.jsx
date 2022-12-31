0b920e65ffd955bed81694ec1aa30173be673991
20120373 <letruongkinhthanh@gmail.com>
2022-12-31 10:39:52 +0700

    chinh sua

--- src/components/cartBook/CartLibrary.jsx
+++ src/components/cartBook/CartLibrary.jsx
@@ -57,7 +57,7 @@ class Main extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="containFav">
         <div className="app-card-list1" id="app-CardBook-list">
           {Object.keys(this.state.posts).map((key) => (
             <CardBook key={key} index={key} details={this.state?.posts[key]} />
