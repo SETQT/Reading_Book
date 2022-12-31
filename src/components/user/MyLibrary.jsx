919c38ef9cbc2d995895f44fa14648a2c07a7e4a
20120373 <letruongkinhthanh@gmail.com>
2022-12-31 11:28:26 +0700

    update history

--- src/components/user/MyLibrary.jsx
+++ src/components/user/MyLibrary.jsx
@@ -5,6 +5,7 @@ import DescribeContentUser from '../content/DescribeContentUser'
 import ContentUser from '../content/ContentUser'
 import "../sidebar/homeUser.css"
 import AlertDialogSlide from '../dialog/Dialog';
+import BeatLoader from "react-spinners/ClipLoader";
 
 import HeaderUser from '../../components/header/HeaderUser'
 
@@ -14,14 +15,31 @@ import { Outlet } from 'react-router-dom'
 
 function MyLibrary() {
     const [book, setBook] = useState(10);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(()=>{
+        setLoading(true)
+        setTimeout(()=>{
+                setLoading(false)
+        },5000)
+    },[])
     return (
          
             <div>
-                <div className={style.libraryheader1}>
+                {
+                        loading?
+                        <BeatLoader color={"D0021B"} loading={loading}  size={150}/>
+                        :
+                        <div>
+                        <div className={style.libraryheader1}>
                            <div className={style.title1}>My history</div>
 
                         </div>
-                    <ContentUser style={{ flex: 1 }} setBook={setBook} />         
+                        <ContentUser style={{ flex: 1 }} setBook={setBook} /> 
+                          </div>
+                }
+                
+                      
             </div>
                     
     )
